f9dc9d2e73448cc97b8374795cb10696133bcb45
HP <HP@DESKTOP-8E0FCDH>
2024-09-17 01:42:08 +0600

    case.js

---
+++ case.js
@@ -0,0 +1,22 @@
+// const scl = "RAJ UK Uttara model scl";
+// console.log(scl);
+// console.log(scl.toLowerCase());
+const sub = "chemistry";
+const book = "Chemistry";
+// upper case
+// lower case
+if (sub.toLocaleLowerCase() === book.toLocaleLowerCase()) {
+  console.log("ami eibar porbo");
+} else {
+  console.log("ami eibar porbo na");
+} 
+
+const nam = "shila";
+const id = " shila";
+// upper case
+// lower case
+if (nam.trim() === id.trim()) {
+  console.log("ami eibar porbo");
+} else {
+  console.log("ami eibar porbo na");
+}
---
+++ string.js
@@ -0,0 +1,10 @@
+// string jachai kora
+
+// array er moddhe koita length
+// const numbers = [34, 56, 67, 45, 456, 4, 64, 4, 36];
+// console.log(numbers.length);
+// console.log(numbers[2]);
+const country = "india";
+console.log(country.length);
+console.log(country[0]);
+console.log()
