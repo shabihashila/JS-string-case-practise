687b35867240cf53d7ea0b150ae312ac3f04d7d8
HP <HP@DESKTOP-8E0FCDH>
2024-09-22 15:27:13 +0600

    concat.js

---
+++ concat.js
@@ -0,0 +1,8 @@
+const first = "shila";
+const last = "jahan";
+
+const fullName = first + " " + last;
+console.log(fullName);
+// console.log(last.includes("n"));
+const hey = last.includes("n");
+console.log(hey);
---
+++ slice.js
@@ -0,0 +1,16 @@
+// const address = "andorkilla";
+// const part = address.slice(2, 5);
+// console.log(part);
+// const library = "china";
+// const part = library.slice(2, 3);
+// console.log(part);
+
+// const sentence = "i ,am, a, good, student ,and working person";
+// const part = sentence.split(",");
+// console.log(part);
+const frdstr = ["shila", "hila", "dila", "eila", "cila", "silaar", "giad"];
+const frd = frdstr.join("#");
+console.log(frd);
+// const frd = "sfg,sth,sgt,sh,sh,sthh,";
+// const frds = frd.split(",");
+// console.log(frds);
