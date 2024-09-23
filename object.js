6e83fd3188cb738ff59e8c867b124ee1a4ca38fb
HP <HP@DESKTOP-8E0FCDH>
2024-09-23 20:04:29 +0600

    object.js

---
+++ object.js
@@ -0,0 +1,15 @@
+const age = 21;
+const school = "mma scl";
+const isPassed = true;
+let isDeveloper;
+const sub = ["bangla", "eng", "phy", "math"];
+
+// object declear
+const student = {
+  name: "shila",
+  id: 43456,
+  height: 5.3,
+  weight: 53,
+  isAttend: false,
+  subject: ["chemistry", "biology", "math"],
+};
