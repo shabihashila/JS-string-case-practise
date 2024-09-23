e39d782b53c3ff97b69bdee236590594e9e0b71e
HP <HP@DESKTOP-8E0FCDH>
2024-09-23 14:57:23 +0600

    reverse.js

---
+++ reverse.js
@@ -0,0 +1,33 @@
+// const sentence = "i am shila";
+// let reverse = "";
+
+// for (const letter of sentence) {
+//   reverse = letter + reverse;
+// }
+// console.log(reverse);
+
+// ignore this solve
+// const sentence = "i am jerin";
+// let rev = "";
+// for (i = 0; i < sentence.length; i++) {
+//   //   console.log(i);
+//   const letter = sentence[i];
+//   rev = letter + rev;
+// }
+// console.log(rev);
+
+// const nam = "i am  yeasin";
+// let rev = "";
+// for (i = 0; i < nam.length; i++) {
+//   console.log(i);
+//   const letter = nam[i];
+//   rev = letter + rev;
+// }
+// console.log(rev);
+const vab = "jerin moni";
+let rev = "";
+for (i = 0; i < vab.length; i++) {
+  const letter = vab[i];
+  rev = letter + rev;
+}
+console.log(rev);
